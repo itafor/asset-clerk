e4c9d46a78dfc7b60d316aa9e0be6d2934eec988
Weje Emmanuel <emmanuel.weje@techadvance.ng>
2019-05-30 13:31:30 +0100

    WIP: payment

---
+++ app/Mail/DueRentTenant.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use App\TenantRent;
+
+class DueRentTenant extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $rental;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct(TenantRent $rental)
+    {
+        $this->rental = $rental;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->view('emails.due_rent_tenant')
+        ->subject('Rent Due in 30 Days');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -79,6 +79,7 @@ Route::group(['middleware' => 'auth'], function () {
 			Route::post('/store', 'RentalController@store')->name('rental.store');
 			Route::get('/approvals', 'RentalController@approvals')->name('rental.approvals');
 			Route::get('/delete/{uuid}', 'RentalController@delete')->name('rental.delete');
+			Route::get('notify-due-rent', 'RentalController@notifyDueRent');
 		});
 		Route::prefix('maintenance')->group(function(){
 			Route::get('/', 'MaintenanceController@index')->name('maintenance.index');
@@ -116,8 +117,6 @@ Route::group(['middleware' => 'auth'], function () {
 		Route::get('maintenance', 'ReportController@maintenance')->name('report.maintenance');
 		Route::get('legal', 'ReportController@legal')->name('report.legal');
 	});
-
-	
 
 	Route::get('fetch-states/{country}', 'UtilsController@fetchState');
 	Route::get('fetch-cities/{state}', 'UtilsController@fetchCity');
