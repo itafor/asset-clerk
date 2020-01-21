3dd49ac654f84f01deaa9861eb0c80971f084182
francis <itaforfrancis@gmail.com>
2020-01-21 15:18:19 -0800

    register updated

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -172,7 +172,7 @@
                         <div class="form-group{{ $errors->has('captcha') ? ' has-danger' : '' }}">
                             <label class="sr-only" for="captcha">Captcha</label>
                             <div class="captcha">
-                                <span>{!!  Captcha::img('inverse') !!}</span>
+                                <span>{!!  Captcha::img() !!}</span>
                                 <button type="button" class="btn btn-success btn-refresh">Refresh</button>
                             </div>
                           <input type="text" class="form-control{{ $errors->has('captcha') ? ' is-invalid' : '' }}"
