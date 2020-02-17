8b34545e073747a888fd84e706272d3cba8e4ec6
francis <itaforfrancis@gmail.com>
2020-02-16 22:15:49 -0800

    progress bar added to dashboard

--- resources/views/new/dashboard.blade.php
+++ resources/views/new/dashboard.blade.php
@@ -1,6 +1,102 @@
 @extends('new.layouts.app', ['title' => 'Dashboard', 'page' => 'dashboard'])
 
 @section('content')
+<style type="text/css">
+/*basic reset*/
+* {margin: 0; padding: 0;}
+
+/*form styles*/
+#msform {
+    width: 800px;
+    margin: 50px auto;
+    text-align: center;
+    position: relative;
+}
+/*progressbar*/
+#progressbar {
+    margin-bottom: 30px;
+    overflow: hidden;
+    /*CSS counters to number the steps*/
+    counter-reset: step;
+}
+#progressbar li {
+    list-style-type: none;
+    color: #003366;
+    text-transform: uppercase;
+    font-size: 16px;
+    width: 20%;
+    float: left;
+    position: relative;
+}
+#progressbar li:before {
+    content: counter(step);
+    counter-increment: step;
+    width: 20px;
+    line-height: 20px;
+    display: block;
+    font-size: 10px;
+    color: #333;
+    background: white;
+    border-radius: 3px;
+    margin: 0 auto 5px auto;
+}
+/*progressbar connectors*/
+#progressbar li:after {
+    content: '';
+    width: 100%;
+    height: 2px;
+    background: white;
+    position: absolute;
+    left: -50%;
+    top: 9px;
+    z-index: -1; /*put it behind the numbers*/
+}
+#progressbar li:first-child:after {
+    /*connector not needed before the first step*/
+    content: none; 
+}
+/*marking active/completed steps green*/
+/*The number of the step and the connector before it = green*/
+#progressbar li.active:before,  #progressbar li.active:after{
+    background: #27AE60;
+    color: white;
+}
+
+a { color: inherit; } 
+
+</style>
+<div class="row">
+    <!-- multistep form -->
+<form id="msform">
+
+    <!-- progressbar -->
+    <ul id="progressbar">
+           <li class="activ">
+            <a href="{{route('landlord.create')}}" title="Add New Landlord">
+           Landlords
+          </a>
+       </li>
+           <li>
+            <a href="{{route('asset.create')}}" title="Add New Asset">
+           Assets
+          </a>
+       </li>
+           <li>
+            <a href="{{route('tenant.create')}}" title="Add New Tenant">
+           Tenants
+          </a>
+       </li>
+          <li>
+             <a href="{{route('rental.create')}}" title="Add New Rental">
+           Rentals
+          </a>
+      </li>
+    </ul>
+
+</form>
+
+</div>
+
     <div class="row">
 
         <div class="col-xl-4 col-sm-6">
@@ -354,3 +450,7 @@
         <!-- /grid item -->
     </div>
 @endsection
+<!-- jQuery -->
+<script src="http://thecodeplayer.com/uploads/js/jquery-1.9.1.min.js" type="text/javascript"></script>
+<!-- jQuery easing plugin -->
+<script src="http://thecodeplayer.com/uploads/js/jquery.easing.min.js" type="text/javascript"></script>
