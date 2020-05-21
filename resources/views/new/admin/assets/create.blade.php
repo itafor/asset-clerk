055ee84553e11bdc2476c46ea5b7a2c7986bdff8
francis <itaforfrancis@gmail.com>
2020-05-20 20:52:22 -0700

    User location update

--- resources/views/new/admin/assets/create.blade.php
+++ resources/views/new/admin/assets/create.blade.php
@@ -100,9 +100,7 @@
                                     <select name="country" id="country" class="form-control" required>
                                         <option value="">Select Country</option>
                                         @foreach (getCountries() as $c)
-                                            <option value="{{$c->id}}" @if(userLocation())
-                                                {{$c->name == userLocation()->countryName ? 'selected' : 'Select Country'}} 
-                                                @endif>{{$c->name}}</option>
+                                            <option value="{{$c->id}}" {{$c->name=='Nigeria'?'Selected':'Select country'}}>{{$c->name}}</option>
                                         @endforeach
                                     </select>
 
